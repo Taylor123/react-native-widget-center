16d204dded48b169a107749276c4c929f15c6a21
Marcos Rodriguez Vélez <marcosrodriguez@me.com>
2020-11-06 21:53:29 -0500

    Don't compile if on Mac Catalyst.
    widgetkit is not currently available on mac catalyst.

--- ios/RNWidgetCenter.swift
+++ ios/RNWidgetCenter.swift
@@ -6,7 +6,9 @@
 //
 
 import Foundation
+#if (arch(arm64) || arch(x86_64)) && !targetEnvironment(macCatalyst)
 import WidgetKit
+#endif
 
 @available(iOS 14, *)
 @objc(RNWidgetCenter)
@@ -18,21 +20,21 @@ class RNWidgetCenter: NSObject {
 	
 	@objc(reloadTimelines:)
 	func reloadTimelines(_ kind: String) {
-		#if arch(arm64) || arch(x86_64)
+		#if (arch(arm64) || arch(x86_64)) && !targetEnvironment(macCatalyst)
 		WidgetCenter.shared.reloadTimelines(ofKind: kind)
 		#endif
 	}
 	
 	@objc(reloadAllTimelines)
 	func reloadAllTimelines() {
-		#if arch(arm64) || arch(x86_64)
+        #if (arch(arm64) || arch(x86_64)) && !targetEnvironment(macCatalyst)
 		WidgetCenter.shared.reloadAllTimelines()
 		#endif
 	}
 
     @objc(getCurrentConfigurations:reject:)
     func getCurrentConfigurations (_ resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) -> Void {
-        #if arch(arm64) || arch(x86_64)
+        #if (arch(arm64) || arch(x86_64)) && !targetEnvironment(macCatalyst)
         WidgetCenter.shared.getCurrentConfigurations {result in
             switch result {
             case .success(let widgetInfo):
