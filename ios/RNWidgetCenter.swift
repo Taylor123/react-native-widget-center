d293b9ec602cd9fb33abda030d86daddbad80d1c
Taylor123 <cjohns28@terpmail.umd.edu>
2021-08-03 16:53:18 -0700

    clean conditionals and bump version

--- ios/RNWidgetCenter.swift
+++ ios/RNWidgetCenter.swift
@@ -5,7 +5,7 @@
 //  Created by Taylor Johnson on 7/20/20.
 //
 import Foundation
-#if (arch(arm64) || arch(x86_64)) || targetEnvironment(macCatalyst)
+#if arch(arm64) || arch(x86_64) || targetEnvironment(macCatalyst)
 import WidgetKit
 #endif
 
@@ -19,21 +19,21 @@ class RNWidgetCenter: NSObject {
 	
 	@objc(reloadTimelines:)
 	func reloadTimelines(_ kind: String) {
-		#if (arch(arm64) || arch(x86_64)) || targetEnvironment(macCatalyst)
+		#if arch(arm64) || arch(x86_64) || targetEnvironment(macCatalyst)
 		WidgetCenter.shared.reloadTimelines(ofKind: kind)
 		#endif
 	}
 	
 	@objc(reloadAllTimelines)
 	func reloadAllTimelines() {
-        #if (arch(arm64) || arch(x86_64)) || targetEnvironment(macCatalyst)
+        #if arch(arm64) || arch(x86_64) || targetEnvironment(macCatalyst)
 		WidgetCenter.shared.reloadAllTimelines()
 		#endif
 	}
 
     @objc(getCurrentConfigurations:reject:)
     func getCurrentConfigurations (_ resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) -> Void {
-        #if (arch(arm64) || arch(x86_64)) || targetEnvironment(macCatalyst)
+        #if arch(arm64) || arch(x86_64) || targetEnvironment(macCatalyst)
         WidgetCenter.shared.getCurrentConfigurations {result in
             switch result {
             case .success(let widgetInfo):
